6c9b7506dd160bae869d85da638f8070e04e4701
RxNsx <alpendev@mail.ru>
2022-07-21 15:45:32 +0700

    LEFT JOIN PRODUCTS CATEGORIES

---
+++ ProductsCategoriesQuery.sql
@@ -0,0 +1,7 @@
+﻿//Выполняем LEFT JOIN так как он идеально подходит под условия задачи - вывести все пары продукт - категория, даже если у продукта нет категории
+//LEFT JOIN поставит в местах где у продукта нету категории значение NULL
+
+
+SELECT Products.name, Category.name
+FROM Products LEFT JOIN  Category ON Products.id=Category.productId 
+ORDER BY Products.name;
